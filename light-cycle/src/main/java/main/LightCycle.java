f58bf8836babe9a6efcaf899b32837e42596bbe9
Christian Scheller <ch.scheller@hotmail.com>
2017-01-12 19:10:11 +0100

    added missing closing bracket

--- light-cycle/src/main/java/main/LightCycle.java
+++ light-cycle/src/main/java/main/LightCycle.java
@@ -204,7 +204,7 @@ public class LightCycle {
             player2Vehicle.addComponent(LightCycleBehaviour.class);
             player2Vehicle.addComponent(BoxCollider.class);//.setBoundingBox(player2VehicleMeshGroup.getBounds());
             maxExtent = Math.max(player2VehicleMeshGroup.getBounds().getExtentX(), Math.max(player2VehicleMeshGroup.getBounds().getExtentY(), player2VehicleMeshGroup.getBounds().getExtentZ()));
-            player2Vehicle.getTransform().setLocal(Mat4.multiply(Mat4.translate(0, -0.5f, 0.5f), Mat4.multiply(Mat4.scale(1f / maxExtent), Mat4.rotate(90,0,0,1), Mat4.rotate(90,0,1,0),Mat4.rotate(180,0,0,1)));
+            player2Vehicle.getTransform().setLocal(Mat4.multiply(Mat4.translate(0, -0.5f, 0.5f), Mat4.multiply(Mat4.scale(1f / maxExtent), Mat4.rotate(90,0,0,1), Mat4.rotate(90,0,1,0),Mat4.rotate(180,0,0,1))));
 
             // player 2 camera wrapper
             GameObject player2CameraWrapper = currentScene.createGameObject();
